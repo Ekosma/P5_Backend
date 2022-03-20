2719c558571829cb0d9a5423969dd4cb0c127241
Evelyn <evelyn.a.kosma@gmail.com>
2022-03-19 22:29:34 -0500

    added model and controller basic structure

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,51 @@
 class LessonsController < ApplicationController
+  # GET /lessons
+  def index
+    @lessons = Lesson.all
+    render json: @lessons
+  end
+
+  # GET /lessons/1
+  def show
+    render json: @lesson
+  end
+
+  # POST /lessons
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      lessons = Lesson.all
+      render json: lessons, status: :created, location: @lessons
+    else
+      render json: @lesson.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /lessons/1
+  def update
+    if @lesson.update(lesson_params)
+      render json: @lesson
+    else
+      render json: @lesson.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /lessons/1
+  def destroy
+    @id = @lesson.id
+    @lesson.destroy
+    lessons = Lesson.all
+    render json: lessons, location: @lessons
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_lesson
+      @lesson = Lesson.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def lesson_params
+      params.permit(:name, :description, :grade, :subject, :content)
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,52 @@
 class ReviewsController < ApplicationController
+  # GET /reviews
+  def index
+    @reviews = Review.all
+    render json: @reviews
+  end
+
+  # GET /reviews/1
+  def show
+    render json: @review
+  end
+
+  # POST /reviews
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      review = Review.where("lesson_id = ?", @review.lesson_id)
+      render json: reviews, status: :created, location: @reviews
+    else
+      render json: @review.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /reviews/1
+  def update
+    if @review.update(review_params)
+      render json: @review
+    else
+      render json: @review.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /reviews/1
+  def destroy
+    @user_id = @review.user_id
+    @review.destroy
+    reviews = Review.where("lesson_id = ?", @lesson_id)
+    render json: reviews, location: @reviews
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def review_params
+      params.permit(:recommended, :comment, :lesson_id)
+    end
+end
 end
